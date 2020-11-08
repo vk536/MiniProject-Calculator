fce01aa89d7249007d446acf88b7ca22e1e3d064
vivek <vk536@njit.edu>
2020-11-08 15:11:16 -0500

    Feature:Adding mean,median and mode functions

---
+++ Statistics/Median.py
@@ -0,0 +1,21 @@
+from Calculator.Division import division
+from Calculator.Subtraction import subtract
+from Calculator.Addition import addition
+
+
+def median(num):
+    try:
+        num_values = len(num)
+        list_num = [num[i] for i in range(num_values)]
+        list_num.sort()
+        if num_values % 2 == 0:
+            median1 = list_num[int(num_values // 2)]
+            median2 = list_num[int(subtract((num_values // 2), 1))]
+            median_result = division(addition(median1, median2), 2)
+        else:
+            median_result = list_num[int(division(num_values, 2))]
+        return median_result
+    except ZeroDivisionError:
+        print("Divide by 0 Error")
+    except ValueError:
+        print("Please Check your data inputs")
